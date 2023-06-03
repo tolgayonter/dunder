9717fb2daf5847b91fbd431adf3fb71ba10976b8
tolgayonter <tolgayonter@gmail.com>
2023-06-03 13:05:32 +0300

    (api) add auth

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,19 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("Default"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
@@ -29,8 +32,13 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-// Add CORS support
+// CORS
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+// Auth
+app.UseAuthentication(); // do you have a valid token?
+app.UseAuthorization(); // okay you have a valid token, now what are you allowed to do?
+
 
 app.MapControllers();
 
