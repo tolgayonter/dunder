cd2cafe5fefa0cebac9e9aab9cc8f5ad1a0f95df
tolgayonter <tolgayonter@gmail.com>
2023-06-16 16:18:49 +0300

    add like feature

---
+++ API/Entities/UserLike.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Entities;
+
+public class UserLike
+{
+    public User SourceUser { get; set; }
+    public int SourceUserId { get; set; }
+    public User TargetUser { get; set; }
+    public int TargetUserId { get; set; }
+}
--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Member } from '../models/member';
+import { MembersService } from '../services/members.service';
+import { Pagination } from '../models/pagination';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.scss']
+  styleUrls: ['./lists.component.scss'],
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit {
+  members: Member[] | undefined;
+  predicate = 'liked';
 
+  pageNumber = 1;
+  pageSize = 5;
+  pagination: Pagination | undefined;
+
+  constructor(private memberService: MembersService) {}
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  loadLikes() {
+    this.memberService
+      .getLikes(this.predicate, this.pageNumber, this.pageSize)
+      .subscribe({
+        next: (response) => {
+          this.members = response.result;
+          this.pagination = response.pagination;
+        },
+      });
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadLikes();
+    }
+  }
 }
