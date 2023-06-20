cb74f61339722611077cd68496da8861863a59a3
tolgayonter <tolgayonter@gmail.com>
2023-06-20 14:59:32 +0300

    feat(client): add admin related stuff - admin panel - edit roles - modal etc.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -11,6 +11,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { preventUnsavedChangesGuard } from './guards/prevent-unsaved-changes.guard';
 import { memberDetailedResolver } from './resolvers/member-detailed.resolver';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { adminGuard } from './guards/admin.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -32,6 +34,11 @@ const routes: Routes = [
       },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
+      {
+        path: 'admin',
+        component: AdminPanelComponent,
+        canActivate: [adminGuard],
+      },
     ],
   },
   { path: 'not-found', component: NotFoundComponent },
--- client/src/app/models/user.ts
+++ client/src/app/models/user.ts
@@ -4,4 +4,5 @@ export interface User {
   photoUrl: string;
   knonwAs: string;
   gender: string;
+  roles: string[];
 }
