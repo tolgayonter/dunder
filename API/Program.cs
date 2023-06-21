635c5fb19de45b94f8231ea661409b3f120a4415
tolgayonter <tolgayonter@gmail.com>
2023-06-21 22:57:17 +0300

    quick fix

--- API/Program.cs
+++ API/Program.cs
@@ -32,7 +32,7 @@ else
     var pgPass = pgUserPass.Split(":")[1];
     var pgHost = pgHostPort.Split(":")[0];
     var pgPort = pgHostPort.Split(":")[1];
-    var updatedHost = pgHost.Replace("flycast", " internal");
+    var updatedHost = pgHost.Replace("flycast", "internal");
     connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
 }
 
